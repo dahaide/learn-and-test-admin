58b536731d3b9a7b6bf0704206f39e20f8adac91
魏新亚 <874817421@qq.com>
2020-05-21 23:08:15 +0800

    取消登录

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,8 @@ const router = new Router({
   routes,
   mode: 'history'
 })
-const LOGIN_PAGE_NAME = 'login'
+// const LOGIN_PAGE_NAME = 'login'
+const LOGIN_PAGE_NAME = 'home'
 
 const turnTo = (to, access, next) => {
   if (canTurnTo(to.name, access, routes)) next() // 有权限，可访问
@@ -24,9 +25,10 @@ router.beforeEach((to, from, next) => {
   const token = getToken()
   if (!token && to.name !== LOGIN_PAGE_NAME) {
     // 未登录且要跳转的页面不是登录页
-    next({
-      name: LOGIN_PAGE_NAME // 跳转到登录页
-    })
+    // next({
+    //   name: LOGIN_PAGE_NAME // 跳转到登录页
+    // })
+    next()
   } else if (!token && to.name === LOGIN_PAGE_NAME) {
     // 未登陆且要跳转的页面是登录页
     next() // 跳转
@@ -45,7 +47,8 @@ router.beforeEach((to, from, next) => {
       }).catch(() => {
         setToken('')
         next({
-          name: 'login'
+          // name: 'login'
+          name: 'home'
         })
       })
     }
